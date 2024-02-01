7604039d7645e7d60b94939f694dd5092a55aeff
Trevor Williams <tmw7991@gmail.com>
2024-02-01 14:26:25 -0700

    fix: move data fetch to server action

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,6 @@
-import { db } from "@/db/firebase";
-import { collection, getDocs } from "firebase/firestore";
+import { fetchData } from "@/db/actions";
 
 import ImageGrid from "@/components/image-grid";
-
-async function fetchData() {
-  try {
-    const colRef = collection(db, "images");
-    const snap = await getDocs(colRef);
-    const data = snap.docs
-      .map((doc) => {
-        return doc.data() as ImageMetadata;
-      })
-      .sort((a, b) => {
-        return (
-          new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime()
-        );
-      });
-
-    return data;
-  } catch (error) {
-    console.error("Error fetching data from Firestore:", error);
-    return [];
-  }
-}
 
 export default async function Home() {
   const data = await fetchData();
--- src/db/actions.ts
+++ src/db/actions.ts
@@ -7,6 +7,27 @@ import {
   deleteField,
 } from "firebase/firestore";
 
+export async function fetchData() {
+  try {
+    const colRef = collection(db, "images");
+    const snap = await getDocs(colRef);
+    const data = snap.docs
+      .map((doc) => {
+        return doc.data() as ImageMetadata;
+      })
+      .sort((a, b) => {
+        return (
+          new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime()
+        );
+      });
+
+    return data;
+  } catch (error) {
+    console.error("Error fetching data from Firestore:", error);
+    return [];
+  }
+}
+
 export async function updateDB() {
   const mainColRef = collection(db, "images");
   const mainColSnapshot = await getDocs(mainColRef);
